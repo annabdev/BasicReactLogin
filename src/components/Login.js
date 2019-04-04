82bf428b34091de5d5d40d15874be01055c13c16
Anna Beagley <annabeagley@Annas-MacBook-Air.local>
2019-04-04 13:02:27 -0600

    Print to console added

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,22 +3,35 @@ import { Link } from "react-router-dom";
 
 
 class Login extends Component {
-  state = {}
+  constructor(props) {
+  super(props);
+  this.state = {email:'',password:''};
+
+  this.handleSubmit = this.handleSubmit.bind(this);
+  
+  }
+  handleSubmit(event) {
+console.log('email: ' + this.state.email,'password: ' +this.state.password);
+event.preventDefault();
+  }
+
   render() {
     return (
       <div>
         <h2>Login</h2>
    
-          <form>
+          <form onSubmit={this.handleSubmit}>
             <div class="form-group">
               <label for="exampleInputEmail1">Email address</label>
               <input type="email" class="form-control-sm" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"
+              onChange={event=> this.setState({email:event.target.value})}
               />
             </div>
             
             <div class="form-group">
               <label for="exampleInputPassword1">Password</label>
               <input type="password" class="form-control-sm" id="exampleInputPassword1" placeholder="Password"
+              onChange={event=> this.setState({password:event.target.value})}
               />
             </div>
             <button type="submit">Login</button>
@@ -30,6 +43,7 @@ class Login extends Component {
 
       </div>
     )}
+  
       };
 
 export default Login;
--- src/components/Register.js
+++ src/components/Register.js
@@ -5,27 +5,43 @@ import { Link } from "react-router-dom"
 
 
 class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {name:'',email:'',password:'',confirm:''};
+  
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+    
+    handleSubmit(event) {
+  console.log('Full Name' + this.state.name,'email: ' +this.state.email,'password: '+this.state.password,'confirm; '+this.state.confirm);
+  event.preventDefault();
+    }
+
   render() {
     return (
       <div>
         <h2>Register</h2>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div class="form-group">
-            <label for="exampleInputEmail1">Full Name</label>
-            <input type="text" class="form-control-sm" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Full Name" />
+            <label for="exampleInputEmail1">Name</label>
+            <input type="text" class="form-control-sm" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Full Name" 
+            onChange={event=> this.setState({name:event.target.value})}/>
 
           </div>
           <div class="form-group">
             <label for="exampleInputEmail1">Email Address</label>
-            <input type="email" class="form-control-sm" id="exampleInputPassword1" placeholder="Email Address" />
+            <input type="email" class="form-control-sm" id="exampleInputPassword1" placeholder="Email Address" 
+            onChange={event=> this.setState({email:event.target.value})}/>
           </div>
           <div class="form-group">
             <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control-sm" id="exampleInputPassword1" placeholder="Password" />
+            <input type="password" class="form-control-sm" id="exampleInputPassword1" placeholder="Password" 
+            onChange={event=> this.setState({password:event.target.value})}/>
           </div>
           <div class="form-group">
             <label for="exampleInputPassword1">Confirm Password</label>
-            <input type="password" class="form-control-sm" id="exampleInputPassword1" placeholder="Password" />
+            <input type="password" class="form-control-sm" id="exampleInputPassword1" placeholder="Password" 
+            onChange={event=> this.setState({confirm:event.target.value})}/>
           </div>
           <button type="submit">Submit</button>
         </form>
